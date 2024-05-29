b1a8a0babb51d7e8f81177e24f4d5a725a703a1c
Gellila Zeru <gellilazeru1@gmail.com>
2024-05-29 16:14:30 +0100

    completed MVP and extensions

--- src/main/java/com/example/task_01/controllers/TaskController.java
+++ src/main/java/com/example/task_01/controllers/TaskController.java
@@ -1,5 +1,6 @@
 package com.example.task_01.controllers;
 
+import com.example.task_01.models.Celebration;
 import com.example.task_01.models.Greeting;
 import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +9,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// default to http://localhost:8080/greetings
-// @RequestMapping(value = "/greeting"): this is for every GET request
+// default to http://localhost:8080/greeting
+@RequestMapping(value = "/greeting")
+//: this is for every GET request
 
 public class TaskController {
 
-    @GetMapping("/greeting")
+    @GetMapping
     public Greeting greeting(@RequestParam(defaultValue = " ") String timeOfDay) {
         Greeting greeting = new Greeting("Colin", timeOfDay);
         return greeting;
+    }
+
+    @GetMapping("/summer")
+    public Celebration celebration(@RequestParam(defaultValue = " ") String message) {
+        Celebration celebration = new Celebration(message);
+        return celebration;
     }
 
 }
